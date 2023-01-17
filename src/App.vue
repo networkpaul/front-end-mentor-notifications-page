36323b388a415b8d024775784fccc1503d6f3420
Emmanuel Paul <emmanuel.paul@networkpaul.com>
2023-01-17 18:46:23 -0500

    updated responsive

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
     </header>
     <div class="notifications-cardsContainer">
       <div v-for="n in notifications" :key="n.id" @click="handleNotificationClick(n.id)" class="notificationContainer" :class="{ 'unread': n.isUnread }">
-        <div class="image-container">
+        <div class="image-container" :class="{ 'alignSelf': n.privateMessage}">
           <img :src="n.author.src" :alt="n.author.name" class="notification-image" />
         </div>
         <div class="text-container">
@@ -29,9 +29,11 @@
           <span class="notification-time">{{ n.time }}</span>
           <p v-if="n.privateMessage" class="notification-message">{{ n.privateMessage }}</p>
         </div>
-        <a v-if="n.image" :href="n.image.href">
-          <img :src="n.image.src" :alt="n.image.alt" />
-        </a>
+        <div v-if="n.image" class="notification-postImage">
+          
+            <img :src="n.image.src" :alt="n.image.alt" />
+          
+        </div>      
       </div>
     </div>
     </div>
@@ -125,6 +127,11 @@ export default {
           "time": "1 week ago",
           "isUnread": false,
           "privateMessage" : "",
+          "image": {
+            "href": "#",
+            "src": require("../src/assets/notifications-page-main/assets/images/image-chess.webp"),
+            "alt": "image-chess.webp",
+          }
         },
         {
           "id": "6",
@@ -187,9 +194,10 @@ export default {
 
 .notifications-container {
   margin: auto;
+  margin-bottom: 100px;
   padding: 30px 20px;
-  min-width: 516px;
-  max-width: 900px;
+  min-width: 375px;
+  max-width: 730px;
   display: flex;
   flex-direction: column;
   align-self: center;
@@ -214,9 +222,9 @@ export default {
 .notifications-header {
   margin: 8px 30px;
   width: 90%;
-  align-self: flex-start;
   display: flex;
   align-items: center;
+  align-self: flex-start;
   justify-content: space-between;
 }
 
@@ -237,8 +245,6 @@ export default {
 }
 
 .notifications-cardsContainer {
-  // margin: 8px 30px;
-  // width: 90%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -372,6 +378,17 @@ export default {
     }
 }
 
+.notification-postImage {
+  width: 10%;
+  min-width: 10%;
+  margin-left: 140px;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+
 .unread {
   background-color: #F7FAFD;
 }
@@ -381,7 +398,8 @@ export default {
     width: 10px;
     background-color: #F65552;
     border-radius: 50%;
-    display: inline-block;
+    display: flex;
+    // order: -1;
 }
 
 .notificationAlert {
@@ -390,6 +408,79 @@ export default {
     background-color: transparent;
     border-radius: 50%;
     display: inline-block;
+}
+
+.alignSelf {
+  align-self: flex-start;
+}
+
+@media only screen and (min-width: 1025px) and (max-width: 1200px) {
+  
+}
+
+@media only screen and (min-width: 769px) and (max-width: 1024px) {
+  
+}
+
+@media only screen and (min-width: 481px) and (max-width: 768px) {
+  
+}
+
+@media only screen and (min-width: 320px) and (max-width: 480px) {
+
+  .notifications-header {
+    margin: 0;
+    margin-bottom: 24px;
+    width: 100%;
+  }
+
+  .notifications-title {
+    margin: 0;
+    font-size: 20px;
+    line-height: 25.2px;
+  }
+
+  .notifications-markAllAsRead {
+    font-size: 14px;
+    line-height: 17px;
+    widows: 100%;
+  }
+
+  .notifications-cardsContainer {
+    margin: 0 16px;
+    width: 100%;
+    gap: 10px;
+  }
+
+  .notificationContainer {
+    margin: 0;
+    padding: 16px;
+  }
+
+  .image-container {
+    min-width: 15%;
+  }
+
+  .notification-name {
+    font-size: 14px;
+    line-height: 17px;
+  }
+
+  .notification-default {
+    font-size: 14px;
+    line-height: 17px;
+  }
+
+  .notification-post {
+    font-size: 14px;
+    line-height: 17px;
+  }
+
+  .notification-time {
+    font-size: 14px;
+    line-height: 17px;
+  }
+
 }
 
 </style>
